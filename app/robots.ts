6ceaffc76a2cbea5e4c5ab468cbf15a19c17823c
alexlamas <alexlama@anthropic.com>
2024-12-13 16:54:52 +0000

    robots

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/api/", "/private/"],
     },
-    sitemap: "https://yourdomain.com/sitemap.xml",
+    sitemap: "https://arabicflashcards.vercel.app/sitemap.xml",
   };
 }
