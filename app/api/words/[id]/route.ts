6d84c1165ff9f194010975e6957882d827d8bb79
alexlamas <alexlama@anthropic.com>
2024-12-15 20:15:08 +0000

    fix import

--- app/api/words/[id]/route.ts
+++ app/api/words/[id]/route.ts
@@ -1,8 +1,6 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
-
-import { type NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: NextRequest,
